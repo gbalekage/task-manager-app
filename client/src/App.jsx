11ebde30c5a1abf7382489134519ce5bec7559cf
Balekage Gael <gbalekage21@gmail.com>
2025-11-20 21:24:43 +0200

    routes setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,19 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import SignInPage from "./pages/auth/sign-in";
+import SignUpPage from "./pages/auth/sign-up";
+import HomePage from "./pages/main/home-page";
 
 const App = () => {
   return (
-    <div className="flex items-center justify-center min-h-screen">App</div>
+    <Routes>
+      <Route path="auth">
+        <Route path="sign-in" element={<SignInPage />} />
+        <Route path="sign-up" element={<SignUpPage />} />
+      </Route>
+
+      <Route path="/" element={<HomePage />} />
+    </Routes>
   );
 };
 
