f28aa36b1bbb8a5d2b8d8d37c5163d654dd1bdcb
Balekage Gael <gbalekage21@gmail.com>
2025-11-21 07:07:49 +0200

    axios setup for client

---
+++ client/src/lib/axios.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "http://localhost:5000/api",
+  withCredentials: true,
+});
+
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      window.location.href = "/auth/sign-in";
+    }
+    return Promise.reject(error);
+  }
+);
+
+module.exports = api;
